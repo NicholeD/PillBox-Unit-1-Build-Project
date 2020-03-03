7cae15a0c0a5ce1a511c22c3adc0089c517fb347
Harmony Radley <harmonyradley99@gmail.com>
2020-03-03 17:41:36 -0500

    Added a Dark Mode Feature to Settings VC Scence and wrote code for it

---
+++ PillBox/PillBox/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  PillBox
+//
+//  Created by Harmony Radley on 3/3/20.
+//  Copyright © 2020 Nichole Davidson. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    func IBActionDarkMode(_ sender: UISwitch) {
+        if (sender.isOn == true) {
+            themeHelper?.setThemePreferenceToDark()
+            dismiss(animated: true, completion: nil)
+        }
+       
+        
+    }
+    
+    var themeHelper: ThemeHelper?
+}
