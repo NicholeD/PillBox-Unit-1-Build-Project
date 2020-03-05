068935136e6e70377a464b32d578489ee1fff608
Nichole Davidson <nicholedavid@gmail.com>
2020-03-04 17:06:19 -0700

    fixing previous commit

---
+++ PillBox/PillBox/Model/DarkModeHelper.swift
@@ -0,0 +1,31 @@
+//
+//  DarkModeHelper.swift
+//  PillBox
+//
+//  Created by Harmony Radley on 3/3/20.
+//  Copyright © 2020 Nichole Davidson. All rights reserved.
+//
+import UIKit
+
+class ThemeHelper {
+    
+        
+    func setThemePreferenceToDark() {
+        UserDefaults.standard.set("Dark", forKey: themePreferenceKey)
+    }
+    
+    var themePreference: String? {
+        return UserDefaults.standard.string(forKey: themePreferenceKey)
+    }
+    
+    
+    
+    init() {
+        guard themePreference == nil else { return }
+        setThemePreferenceToDark()
+    
+        }
+    
+    private let themePreferenceKey = "themePreference"
+
+}
